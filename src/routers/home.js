922d880ad57b5e59ad62d9ca49abecaef40d9d80
Monty <montelion.github@gmail.com>
2021-04-28 20:35:05 +0200

    Added localization helper

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,16 +1,37 @@
 const getLocale = require('../../util/getLocale');
 const { Router } = require('express');
 const router = new Router();
+const fetch = require('node-fetch');
 
 const pgoressLists = require('../progress-lists');
 
+
+
 router.get('/', (req, res) => {
-	const tmpLocale = getLocale('US', 'en');
-	res.render('home', {
-		layout: 'main',
-		locale: tmpLocale,
-		featuredFeatureList: pgoressLists[0]
-	});
+
+	if (req.query.url) {
+
+		fetch(req.query.url)
+			.then((response) => {
+				return response.json()
+			})
+			.then((data) => {
+				let tmpLocale = data;
+				res.render('home', {
+					layout: 'main',
+					locale: tmpLocale,
+					featuredFeatureList: pgoressLists[0],
+					queryUrl: req.query.url
+				});
+			})
+	} else {
+		let tmpLocale = getLocale('US', 'en')
+		res.render('home', {
+			layout: 'main',
+			locale: tmpLocale,
+			featuredFeatureList: pgoressLists[0]
+		});
+	}
 });
 
 module.exports = router;
--- src/routers/progress.js
+++ src/routers/progress.js
@@ -1,16 +1,37 @@
 const getLocale = require('../../util/getLocale');
 const { Router } = require('express');
 const router = new Router();
+const fetch = require('node-fetch');
 
 const progressLists = require('../progress-lists');
 
 router.get('/', (req, res) => {
-	const tmpLocale = getLocale('US', 'en');
-	res.render('progress', {
-		layout: 'main',
-		locale: tmpLocale,
-		progressLists
-	});
+
+
+	if (req.query.url) {
+
+		fetch(req.query.url)
+			.then((response) => {
+				return response.json()
+			})
+			.then((data) => {
+				let tmpLocale = data;
+				res.render('progress', {
+					layout: 'main',
+					locale: tmpLocale,
+					progressLists,
+					queryUrl: req.query.url
+				});
+			})
+	} else {
+		let tmpLocale = getLocale('US', 'en')
+		res.render('progress', {
+			layout: 'main',
+			locale: tmpLocale,
+			progressLists,
+			queryUrl: req.query.url
+		});
+	}
 });
 
 module.exports = router;
