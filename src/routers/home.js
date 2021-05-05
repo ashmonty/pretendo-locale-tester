8778d814d5694c3d486e08db1c3795bea836c2bd
Monty <monty.github@gmail.com>
2021-05-05 12:06:13 +0200

    𝔗𝔥𝔢 𝔴𝔢𝔟𝔰𝔦𝔱𝔢 𝔦𝔰 𝔫𝔬𝔴 𝔨𝔦𝔫𝔡 𝔢𝔫𝔬𝔲𝔤𝔥 𝔱𝔬 𝔱𝔢𝔩𝔩 𝔶𝔬𝔲 𝔱𝔥𝔞𝔱 𝔶𝔬𝔲'𝔳𝔢 𝔩𝔬𝔠𝔞𝔩𝔦𝔷𝔢𝔡 𝔱𝔥𝔢 𝔴𝔯𝔬𝔫𝔤 𝔣𝔦𝔩𝔢
    𝓲𝓭𝓲𝓸𝓽

--- src/routers/home.js
+++ src/routers/home.js
@@ -1,53 +1,57 @@
-const getLocale = require('../../util/getLocale');
-const { Router } = require('express');
+const getLocale = require("../../util/getLocale");
+const { Router } = require("express");
 const router = new Router();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const progressLists = require('../progress-lists');
+const progressLists = require("../progress-lists");
 
+router.get("/", (req, res) => {
+  if (req.query.url) {
+    let url = req.query.url;
 
+    if (!url.startsWith("http")) {
+      url = `https://${url}`;
+    }
 
-router.get('/', (req, res) => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((locale) => {
 
-	if (req.query.url) {
-
-		let url = req.query.url
-
-		if (!url.startsWith('http')) {
-			url = `https://${url}`
-		}
-
-		fetch(url)
-			.then((response) => {
-				return response.json()
-			})
-			.then((data) => {
-				let tmpLocale = data;
-				res.render('home', {
-					layout: 'main',
-					locale: tmpLocale,
-					featuredFeatureList: progressLists[0],
-					queryUrl: url
-				});
-			})
-			.catch((error) => {
-				let tmpLocale = getLocale('US', 'en')
-				res.render('error', {
-					layout: 'main',
-					locale: tmpLocale,
-					featuredFeatureList: progressLists[0],
-					error
-				})
-			})
-	}
-	else {
-		let tmpLocale = getLocale('US', 'en')
-		res.render('home', {
-			layout: 'main',
-			locale: tmpLocale,
-			featuredFeatureList: progressLists[0]
-		});
-	}
+        if (locale.progressPage) {
+          res.render("home", {
+            layout: "main",
+            locale,
+            featuredFeatureList: progressLists[0],
+            queryUrl: url,
+          });
+        } else {
+          let tmpLocale = getLocale("US", "en");
+          res.render("error", {
+            layout: "main",
+            locale: tmpLocale,
+            error:
+              "Are you sure you're localizing the right file? Make sure you're localizing the file in the dev branch.",
+          });
+        }
+      })
+      .catch((error) => {
+        let tmpLocale = getLocale("US", "en");
+        res.render("error", {
+          layout: "main",
+          locale: tmpLocale,
+          error,
+        });
+      });
+  } else {
+    let tmpLocale = getLocale("US", "en");
+    res.render("home", {
+      layout: "main",
+      locale: tmpLocale,
+      featuredFeatureList: progressLists[0],
+    });
+  }
 });
 
 module.exports = router;
--- src/routers/progress.js
+++ src/routers/progress.js
@@ -1,53 +1,56 @@
-const getLocale = require('../../util/getLocale');
-const { Router } = require('express');
+const getLocale = require("../../util/getLocale");
+const { Router } = require("express");
 const router = new Router();
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const progressLists = require('../progress-lists');
+const progressLists = require("../progress-lists");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  if (req.query.url) {
+    let url = req.query.url;
 
+    if (!url.startsWith("http")) {
+      url = `https://${url}`;
+    }
 
-	if (req.query.url) {
-
-
-		let url = req.query.url
-
-		if (!url.startsWith('http')) {
-			url = `https://${url}`
-		}
-
-		fetch(url)
-			.then((response) => {
-				return response.json()
-			})
-			.then((data) => {
-				let tmpLocale = data;
-				res.render('progress', {
-					layout: 'main',
-					locale: tmpLocale,
-					progressLists,
-					queryUrl: url
-				});
-			})
-			.catch((error) => {
-				let tmpLocale = getLocale('US', 'en')
-				res.render('error', {
-					layout: 'main',
-					locale: tmpLocale,
-					progressLists,
-					error
-				})
-			})
-	}
-	else {
-		let tmpLocale = getLocale('US', 'en')
-		res.render('progress', {
-			layout: 'main',
-			locale: tmpLocale,
-			progressLists
-		});
-	}
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((locale) => {
+        if (locale.progressPage) {
+          res.render("progress", {
+            layout: "main",
+            locale,
+            progressLists,
+            queryUrl: url,
+          });
+        } else {
+          let tmpLocale = getLocale("US", "en");
+          res.render("error", {
+            layout: "main",
+            locale: tmpLocale,
+            error:
+              "Are you sure you're localizing the right file? Make sure you're localizing the file in the dev branch.",
+          });
+        }
+      })
+      .catch((error) => {
+        let tmpLocale = getLocale("US", "en");
+        res.render("error", {
+          layout: "main",
+          locale: tmpLocale,
+          error,
+        });
+      });
+  } else {
+    let tmpLocale = getLocale("US", "en");
+    res.render("progress", {
+      layout: "main",
+      locale: tmpLocale,
+      progressLists,
+    });
+  }
 });
 
 module.exports = router;
